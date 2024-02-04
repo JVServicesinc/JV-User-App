78146957c0a86f46d462055181fc424016fd09e8
Akhil <ezxpertapp@gmail.com>
2024-02-04 22:04:07 +0530

    iOS Folder added.

---
+++ ios/user/AppDelegate.mm
@@ -0,0 +1,29 @@
+#import "AppDelegate.h"
+#import <GoogleMaps/GoogleMaps.h>
+#import "Firebase/Firebase.h"
+#import <React/RCTBundleURLProvider.h>
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  [FIRApp configure];
+  [GMSServices provideAPIKey:@"AIzaSyAo9cqMY4p9ob2K6cvvw2x7ngEy2QHoKLo"];
+  self.moduleName = @"user";
+  // You can add your custom initial props in the dictionary below.
+  // They will be passed down to the ViewController used by React Native.
+  self.initialProps = @{};
+
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+{
+#if DEBUG
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+#else
+  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
+}
+
+@end
